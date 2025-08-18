decff9d5cbfb9eba3db0e5b040d562aecc306685
samiksha pawar <sameekshapawar06@gmail.com>
2025-08-18 21:28:35 +0530

    third commit

---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,30 @@
+import React from 'react'
+import './Home.css'
+
+const Home = () => {
+  return (
+    <div className='home'>
+        <div className="hero">
+            <h1>Largest <br/> Crypto Marketplace</h1>
+            <p>Welcome to the world's largest cryptocurrency marketplace.
+                Sign up to explore more about the crypto world and trade your favorite coins with ease.
+            </p>
+            <form>
+                <input type='text' placeholder='Search crypto...'></input>
+                <button type='submit'>Search</button>
+            </form>
+        </div>
+        <div className="crypto-table">
+            <div className="table-layout">
+                <p>#</p>
+                <p>Coins</p>
+                <p>Price</p>
+                <p>24H Change</p>
+                <p>Market Cap</p>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Home
