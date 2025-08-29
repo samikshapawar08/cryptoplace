a1595520aad16345a784d4a11d48e8aec8080683
samiksha pawar <sameekshapawar06@gmail.com>
2025-08-29 13:46:57 +0530

    eighth commit

--- src/pages/Coin/Coin.jsx
+++ src/pages/Coin/Coin.jsx
@@ -1,9 +1,60 @@
-import React from 'react'
-import './Coin.css'
-const Coin = () => {
-  return (
-    <div>Coin</div>
-  )
-}
+import React, { useContext, useEffect, useState } from "react";
+import "./Coin.css";
+import { useParams } from "react-router-dom";
+import { CoinContext } from "../../context/CoinContext";
+import LineChart from "../../components/LineChart/LineChart";
 
-export default Coin
+const Coin = () => {
+  const { coinId } = useParams();
+  const [coinData, setCoinData] = useState();
+  const { currency } = useContext(CoinContext);
+  const [historicalData, setHistoricalData] = useState();
+
+  const fetchCoinData = async () => {
+    const options = { method: "GET", headers: { accept: "application/json" } };
+
+    fetch(`https://api.coingecko.com/api/v3/coins/${coinId}`, options)
+      .then((res) => res.json())
+      .then((res) => setCoinData(res))
+      .catch((err) => console.error(err));
+  };
+
+  const fetchHistoricalData = async () => {
+    const options = { method: "GET", headers: { accept: "application/json" } };
+
+    fetch(
+      `https://api.coingecko.com/api/v3/coins/${coinId}/market_chart?vs_currency=${currency.name}&days=10`,
+      options
+    )
+      .then((res) => res.json())
+      .then((res) => setHistoricalData(res))
+      .catch((err) => console.error(err));
+  };
+  useEffect(() => {
+    fetchCoinData();
+    fetchHistoricalData();
+  }, [currency]);
+  if (coinData && historicalData) {
+    return (
+      <div className="coin">
+        <div className="coin-name">
+          <img src={coinData.image.large} alt="" />
+          <p>
+            <b>
+              {coinData.name} ({coinData.symbol.toUpperCase()})
+            </b>
+          </p>
+        </div>
+        <div className="coin-chart">
+          <LineChart historicalData={historicalData}/>
+        </div>
+      </div>
+    );
+  } else {
+    <div className="spinner">
+      <div className="spin"></div>
+    </div>;
+  }
+};
+
+export default Coin;
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,6 +1,8 @@
 import React, { useContext, useEffect, useState}from 'react'
 import './Home.css'
 import { CoinContext } from '../../context/CoinContext';
+import { Link } from 'react-router-dom';
+
 
 const Home = () => {
   const { allCoin, currency } = useContext(CoinContext);
@@ -9,8 +11,20 @@ const Home = () => {
 
   const inputHandler = (event)=>{
     setInput(event.target.value);
+    if(event.target.value === ""){
+        setDisplayCoin(allCoin);
+    }
 
   }
+  const searchHandler= async(event)=>{
+    event.preventDefault();
+   const coins =  await allCoin.filter((item)=>{
+        return item.name.toLowerCase().includes(input.toLowerCase());
+  })
+  setDisplayCoin(coins);
+}
+
+
 
   useEffect(()=>{
     setDisplayCoin(allCoin);
@@ -22,8 +36,11 @@ const Home = () => {
             <p>Welcome to the world's largest cryptocurrency marketplace.
                 Sign up to explore more about the crypto world and trade your favorite coins with ease.
             </p>
-            <form>
-                <input onChange={inputHandler} value={input}type='text' placeholder='Search crypto...'></input>
+            <form onSubmit={searchHandler}>
+                <input onChange={inputHandler} list='coinlist'value={input} type='text' placeholder='Search crypto...' required></input>
+                <datalist id='coinlist'>
+                    {allCoin.map((item, index)=>(<option key={index} value={item.name}/>))}
+                </datalist>
                 <button type='submit'>Search</button>
             </form>
         </div>
@@ -37,7 +54,7 @@ const Home = () => {
             </div>
             {
                 displayCoin.slice(0,10).map((item, index)=>(
-                    <div className="table-layout" key={index}>
+                    <Link to={`/coin/${item.id}`}className="table-layout" key={index}>
                         <p>{item.market_cap_rank}</p>
                         <div>
                             <img src={item.image} alt={item.name} />
@@ -46,7 +63,7 @@ const Home = () => {
                         <p>{currency.symbol}{item.current_price.toLocaleString()}</p>
                         <p className={item.price_change_percentage_24h>0?"green":"red"}>{Math.floor(item.price_change_percentage_24h*100)/100}</p>
                         <p className='market-cap'>{currency.symbol}{item.market_cap.toLocaleString()}</p>
-                    </div>
+                    </Link>
                 ))
             }
         </div>
