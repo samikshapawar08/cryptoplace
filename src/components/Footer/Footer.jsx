1fb2fff5732c0e833d98e1281c3f7e12675369e6
samiksha pawar <sameekshapawar06@gmail.com>
2025-09-15 20:49:14 +0530

    edited footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -4,7 +4,7 @@ import './Footer.css'
 const Footer = () => {
   return (
     <div className='footer'>
-      <p>© 2025 CryptoPlace. All rights reserved.</p>
+      <p>© 2025 CryptoPlace. All rights reserved. Made by samikshapawar08</p>
     </div>
   )
 }
